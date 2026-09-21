6070950c3063bf5cca3c588a6dd2135995484063
arijukzasprofaz-ui <arijukzasprofaz@gmail.com>
2026-09-21 14:36:36 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import discord
 from discord.ext import commands
-import re, math, io
+import re, math, io, os
 
-TOKEN = "YOUR_BOT_TOKEN_HERE"
+TOKEN = os.environ.get("TOKEN")
 
 # ─── DECODER ───────────────────────────────────────────────────
 
